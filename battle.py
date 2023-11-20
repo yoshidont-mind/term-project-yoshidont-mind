fd74abe69ce3bba7844a9da5cf0004649c16f382
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-20 10:11:51 -0800

    FIX warnings

---
+++ battle.py
@@ -0,0 +1,137 @@
+import random
+
+import characters
+
+
+def append_pokemon(character, pokemon_number, level, hp):
+    pokemon_dic = {'Number': pokemon_number,
+                   'Name': characters.poke_dex()[pokemon_number]['Name'],
+                   'Level': level,
+                   'Max HP': calculate_max_hp(pokemon_number, level),
+                   'HP': hp,
+                   'Attack': calculate_attack(pokemon_number, level),
+                   'Defense': calculate_defense(pokemon_number, level),
+                   'Exp to next level': calculate_exp_to_next_level(level),
+                   'Exp': 0}
+    character['Pokemon'].append(pokemon_dic)
+
+
+def calculate_max_hp(pokemon_number, level):
+    base_stats = characters.poke_dex()[pokemon_number]['HP']
+    max_hp = round((base_stats * 2 + 47) * level / 100 + 10 + level)
+    return max_hp
+
+
+def calculate_attack(pokemon_number, level):
+    base_stats = characters.poke_dex()[pokemon_number]['Attack']
+    attack = round((base_stats * 2 + 47) * level / 100 + 5)
+    return attack
+
+
+def calculate_defense(pokemon_number, level):
+    base_stats = characters.poke_dex()[pokemon_number]['Defense']
+    defense = round((base_stats * 2 + 47) * level / 100 + 5)
+    return defense
+
+
+def calculate_exp_to_next_level(level):
+    return round((level ** 3) * 0.8)
+
+
+def calculate_acquiring_exp(level):
+    return round(level * 150 / 7)
+
+
+def check_for_wild_pokemon():
+    random_number = random.randint(1, 100)
+    if random_number <= 25:
+        return True
+    else:
+        return False
+
+
+def determine_wild_pokemon():
+    pokemon_number = random.randint(1, 4)
+    return pokemon_number
+
+
+def pokemon_catch(foe):
+    random_number = random.randint(1, round(100 * foe['HP'] / foe['Max HP']))
+    if random_number <= 10:
+        return True
+    else:
+        return False
+
+
+def run_success(my_pokemon, foe):
+    random_number = random.randint(1, min(1, 100 - 20 * (my_pokemon['Level'] - foe['Level'])))
+    if random_number <= 20:
+        return True
+    else:
+        return False
+
+
+def attacks(offense, defense):
+    print(f"\n{offense['Name']} attacks {defense['Name']}!")
+    move_power = 40
+    damage = round((offense['Level'] * 2 / 5 + 2) * move_power * offense['Attack'] / defense['Defense'] / 50 + 2)
+    print(f"The {defense['Name']} is damaged by {damage}!")
+    defense['HP'] = max(defense['HP'] - damage, 0)
+    print(f"HP of {defense['Name']}: {defense['HP']}/{defense['Max HP']}")
+
+
+def is_alive(pokemon):
+    if pokemon['HP'] > 0:
+        return True
+    else:
+        return False
+
+
+def check_alive_pokemon_remains(character):
+    for index in range(len(character['Pokemon'])):
+        if character['Pokemon'][index]['HP'] > 0:
+            return True
+    return False
+
+
+def next_pokemon(character):
+    # 生きてるポケモンいることが前提条件
+    # 生きてるポケモンのうち先頭のものを返す
+    for index in range(len(character['Pokemon'])):
+        if character['Pokemon'][index]['HP'] > 0:
+            return character['Pokemon'][index]
+    return False
+
+
+def determine_level_up(pokemon):
+    while pokemon['Exp to next level'] <= 0:
+        pokemon['Level'] += 1
+        print(f"\nCongratulations! {pokemon['Name']} raised to level {pokemon['Level']}!\n")
+        pokemon['Exp'] = -pokemon['Exp to next level']
+        pokemon['Exp to next level'] = calculate_exp_to_next_level(pokemon['Level']) - pokemon['Exp']
+
+        new_attack = calculate_attack(pokemon['Number'], pokemon['Level'])
+        print(f"Attack raised by {new_attack - pokemon['Attack']}.")
+        pokemon['Attack'] = new_attack
+
+        new_defense = calculate_defense(pokemon['Number'], pokemon['Level'])
+        print(f"Defense raised by {new_defense - pokemon['Defense']}.")
+        pokemon['Defense'] = new_defense
+
+        new_max_hp = calculate_max_hp(pokemon['Number'], pokemon['Level'])
+        max_hp_difference = new_max_hp - pokemon['Max HP']
+        print(f"Max HP raised by {max_hp_difference}.\n")
+        pokemon['Max HP'] = new_max_hp
+
+        pokemon['HP'] += max_hp_difference
+
+
+def see_pokemons(character, my_pokemon):
+    print(f"\n----------")
+    print(f"Now in battle: {my_pokemon['Name']}")
+    print("\nPokemon:")
+    for index in range(len(character['Pokemon'])):
+        print(f"{index + 1}) {character['Pokemon'][index]['Name']}")
+        print(f" - Level  : {character['Pokemon'][index]['Level']}")
+        print(f" - HP     : {character['Pokemon'][index]['HP']} / {character['Pokemon'][index]['Max HP']}\n")
+    print(f"----------\n")
--- game.py
+++ game.py
@@ -1,142 +1,6 @@
 import time
-import random
 
-import characters
-import battle
 import event
-
-
-def append_pokemon(character, pokemon_number, level, hp):
-    pokemon_dic = {'Number': pokemon_number,
-                   'Name': characters.poke_dex()[pokemon_number]['Name'],
-                   'Level': level,
-                   'Max HP': calculate_max_hp(pokemon_number, level),
-                   'HP': hp,
-                   'Attack': calculate_attack(pokemon_number, level),
-                   'Defense': calculate_defense(pokemon_number, level),
-                   'Exp to next level': calculate_exp_to_next_level(level),
-                   'Exp': 0}
-    character['Pokemon'].append(pokemon_dic)
-
-
-def calculate_max_hp(pokemon_number, level):
-    base_stats = characters.poke_dex()[pokemon_number]['HP']
-    max_hp = round((base_stats * 2 + 47) * level / 100 + 10 + level)
-    return max_hp
-
-
-def calculate_attack(pokemon_number, level):
-    base_stats = characters.poke_dex()[pokemon_number]['Attack']
-    attack = round((base_stats * 2 + 47) * level / 100 + 5)
-    return attack
-
-
-def calculate_defense(pokemon_number, level):
-    base_stats = characters.poke_dex()[pokemon_number]['Defense']
-    defense = round((base_stats * 2 + 47) * level / 100 + 5)
-    return defense
-
-
-def calculate_exp_to_next_level(level):
-    return round((level ** 3) * 0.8)
-
-
-def calculate_acquiring_exp(level):
-    return round(level * 150 / 7)
-
-
-# events > events
-def adventure_preparation(map_dic, character):
-    print(f"\nDr.Chris \"Hi {character['Name']}!",
-          "         The outside is full of pokemons. They sometimes attack you!",
-          "         So, you should bring your own pokemon.",
-          "         Now, I give you a pokemon.",
-          "         Which one do you choose?\"", sep="\n")
-    numbers_expected = ["1", "2", "3", "4"]
-    make_decision = False
-    while not make_decision:
-        print("\nHere are four pokemons: 1) Bulbasaur, 2) Charmander, 3) Squirtle, 4) Pikachu\n")
-        user_input = input("Please enter your choice:\n")
-        if user_input in numbers_expected:
-            pokemon_ascii_art = characters.poke_dex()[int(user_input)]['Ascii art']
-            print(pokemon_ascii_art)
-            print(f"Are you sure to choose {characters.poke_dex()[int(user_input)]['Name']}?")
-            print("1) Yes 2) No\n")
-            user_confirm = input("Please enter your choice:\n")
-            if user_confirm == "1":
-                print(f"\nDr.Chris \"Good choice! {character['Name']}!\"")
-                append_pokemon(character, int(user_input), 5, calculate_max_hp(int(user_input), 5))
-                print(f"You've gotten {characters.poke_dex()[int(user_input)]['Name']}!\n")
-                make_decision = True
-                update_map_char(map_dic, character['Location'], " ")
-                update_map_value(map_dic, character['Location'], "")
-            else:
-                continue
-        else:
-            print("\nYou're choice is not valid. Please try it again.\n")
-
-
-def lion_gate_bridge(character):
-    print("\nFor now, you cannot proceed further.\n",
-          "Please look forward to future developments.\n")
-    character["Location"] = (5, 8)
-
-
-def lonsdale_quay():
-    print("\n\"Here is 'Lonsdale Quay', the north shore terminal of 'SeaBus'.\"")
-    print("Clerk at the gate \"Sorry, the SeaBus is out of service, now.\"\n")
-
-
-def mount_cypress(character):
-    print("\n\"Here is 'Cypress Mountain', where top pokemon trainers gather.\"")
-    print("For now, you cannot proceed further.\n")
-    character["Location"] = (1, 5)
-
-
-# events
-def check_for_wild_pokemon():
-    random_number = random.randint(1, 100)
-    if random_number <= 25:
-        return True
-    else:
-        return False
-
-
-def determine_wild_pokemon():
-    pokemon_number = random.randint(1, 4)
-    return pokemon_number
-
-
-def pokemon_catch(foe):
-    random_number = random.randint(1, round(100 * foe['HP'] / foe['Max HP']))
-    if random_number <= 10:
-        return True
-    else:
-        return False
-
-
-def run_success(my_pokemon, foe):
-    random_number = random.randint(1, min(1, 100 - 20 * (my_pokemon['Level'] - foe['Level'])))
-    if random_number <= 20:
-        return True
-    else:
-        return False
-
-
-def attacks(offense, defense):
-    print(f"\n{offense['Name']} attacks {defense['Name']}!")
-    move_power = 40
-    damage = round((offense['Level'] * 2 / 5 + 2) * move_power * offense['Attack'] / defense['Defense'] / 50 + 2)
-    print(f"The {defense['Name']} is damaged by {damage}!")
-    defense['HP'] = max(defense['HP'] - damage, 0)
-    print(f"HP of {defense['Name']}: {defense['HP']}/{defense['Max HP']}")
-
-
-def is_alive(pokemon):
-    if pokemon['HP'] > 0:
-        return True
-    else:
-        return False
 
 
 def go_home(character):
@@ -157,178 +21,7 @@ def change_order(character, index):
 
 def escape_pokemon(character, index):
     character['Pokemon'].pop(index)
-
-
-def check_alive_pokemon_remains(character):
-    for index in range(len(character['Pokemon'])):
-        if character['Pokemon'][index]['HP'] > 0:
-            return True
-    return False
-
-
-def next_pokemon(character):
-    # 生きてるポケモンいることが前提条件
-    # 生きてるポケモンのうち先頭のものを返す
-    for index in range(len(character['Pokemon'])):
-        if character['Pokemon'][index]['HP'] > 0:
-            return character['Pokemon'][index]
-    return False
-
-
-def determine_level_up(pokemon):
-    while pokemon['Exp to next level'] <= 0:
-        pokemon['Level'] += 1
-        print(f"\nCongratulations! {pokemon['Name']} raised to level {pokemon['Level']}!\n")
-        pokemon['Exp'] = -pokemon['Exp to next level']
-        pokemon['Exp to next level'] = calculate_exp_to_next_level(pokemon['Level']) - pokemon['Exp']
-
-        new_attack = calculate_attack(pokemon['Number'], pokemon['Level'])
-        print(f"Attack raised by {new_attack - pokemon['Attack']}.")
-        pokemon['Attack'] = new_attack
-
-        new_defense = calculate_defense(pokemon['Number'], pokemon['Level'])
-        print(f"Defense raised by {new_defense - pokemon['Defense']}.")
-        pokemon['Defense'] = new_defense
-
-        new_max_hp = calculate_max_hp(pokemon['Number'], pokemon['Level'])
-        max_hp_difference = new_max_hp - pokemon['Max HP']
-        print(f"Max HP raised by {max_hp_difference}.\n")
-        pokemon['Max HP'] = new_max_hp
-
-        pokemon['HP'] += max_hp_difference
-
-
-def see_pokemons(character, my_pokemon):
-    print(f"\n----------")
-    print(f"Now in battle: {my_pokemon['Name']}")
-    print("\nPokemon:")
-    for index in range(len(character['Pokemon'])):
-        print(f"{index + 1}) {character['Pokemon'][index]['Name']}")
-        print(f" - Level  : {character['Pokemon'][index]['Level']}")
-        print(f" - HP     : {character['Pokemon'][index]['HP']} / {character['Pokemon'][index]['Max HP']}\n")
-    print(f"----------\n")
-
-
-def event_wilderness(character):
-    if check_for_wild_pokemon():
-        pokemon_number = determine_wild_pokemon()
-        my_pokemon = next_pokemon(character)
-        foe_level = random.randint(2, my_pokemon['Level'] + 2)
-        foe = {'Number': pokemon_number,
-               'Name': characters.poke_dex()[pokemon_number]['Name'],
-               'Level': foe_level,
-               'Max HP': calculate_max_hp(pokemon_number, foe_level),
-               'HP': calculate_max_hp(pokemon_number, foe_level),
-               'Attack': calculate_attack(pokemon_number, foe_level),
-               'Defense': calculate_defense(pokemon_number, foe_level)}
-        pokemon_ascii_art = characters.poke_dex()[pokemon_number]['Ascii art']
-        print(pokemon_ascii_art)
-        print(f"Wild {foe['Name']} (Lv. {foe['Level']}) appeared!")
-        print(f"\nLet's go, {my_pokemon['Name']}!")
-        event_in_progress = True
-        while event_in_progress:
-            user_input = input("\nWhat do you do?: 1) Fight, 2) Catch, 3) See pokemons, 4) Run\n")
-            numbers_expected = ["1", "2", "3", "4"]
-            if user_input in numbers_expected:
-                if user_input == "1":
-                    attacks(my_pokemon, foe)
-                    if is_alive(foe):
-                        attacks(foe, my_pokemon)
-                        if not is_alive(my_pokemon):
-                            print(f"\n{my_pokemon['Name']} is defeated!")
-                            if check_alive_pokemon_remains(character):
-                                my_pokemon = next_pokemon(character)
-                                print(f"\nLet's go, {my_pokemon['Name']}!")
-                            else:
-                                print("All pokemons are defeated.",
-                                      "You rush home...", sep="\n")
-                                time.sleep(2)
-                                event_in_progress = False
-                                go_home(character)
-                    else:
-                        print(f"\n{my_pokemon['Name']} beat {foe['Name']}!",
-                              f"\n{my_pokemon['Name']} got {calculate_acquiring_exp(foe_level)}exp.\n")
-                        my_pokemon['Exp'] += calculate_acquiring_exp(foe_level)
-                        my_pokemon['Exp to next level'] -= calculate_acquiring_exp(foe_level)
-
-                        # determine level up
-                        determine_level_up(my_pokemon)
-                        event_in_progress = False
-                elif user_input == "2":
-                    if len(character['Pokemon']) >= 6:
-                        print("\nYou cannot bring more than six pokemons!")
-                    elif pokemon_catch(foe):
-                        append_pokemon(character, pokemon_number, foe_level, foe['HP'])
-                        print(f"Congratulations! You've caught {foe['Name']} successfully!\n")
-                        event_in_progress = False
-                    else:
-                        print(f"Woops, you failed to catch {foe['Name']}.")
-                        attacks(foe, my_pokemon)
-                        if not is_alive(my_pokemon):
-                            print(f"\n{my_pokemon['Name']} is defeated!")
-                            if check_alive_pokemon_remains(character):
-                                my_pokemon = next_pokemon(character)
-                                print(f"\nLet's go, {my_pokemon['Name']}!")
-                            else:
-                                print("All pokemons are defeated.",
-                                      "You rush home...", sep="\n")
-                                time.sleep(2)
-                                event_in_progress = False
-                                go_home(character)
-                elif user_input == "3":
-                    see_pokemons(character, my_pokemon)
-                elif user_input == "4":
-                    if run_success(my_pokemon, foe):
-                        print(f"You've successfully run away from {foe['Name']}!\n")
-                        event_in_progress = False
-                    else:
-                        print(f"Woops, you failed to run from {foe['Name']}.")
-                        attacks(foe, my_pokemon)
-                        if not is_alive(my_pokemon):
-                            print(f"\n{my_pokemon['Name']} is defeated!")
-                            if check_alive_pokemon_remains(character):
-                                my_pokemon = next_pokemon(character)
-                                print(f"\n Let's go, {my_pokemon['Name']}!")
-                            else:
-                                print("All pokemons are defeated.",
-                                      "You rush home...", sep="\n")
-                                time.sleep(2)
-                                event_in_progress = False
-                                go_home(character)
-            else:
-                print("\nYou're choice is not valid. Please try it again.\n")
-
-
-def event(map_dic, character):
-    if character['Location'] == (17, 4):
-        adventure_preparation(map_dic, character)
-    elif character['Location'] == (14, 6):
-        lonsdale_quay()
-    elif character['Location'] == (5, 9):
-        lion_gate_bridge(character)
-    elif character['Location'] == (1, 4):
-        mount_cypress(character)
-    else:
-        pass
-
-
-def event_information(character):
-    if character['Location'] == (5, 8):
-        print("\n\"Here is 'Lion Gate Bridge'. The gateway to the world.\"\n")
-    elif character['Location'] == (12, 4):
-        print("\n\"Here is 'Grouse Mountain'. Be careful for strong pokemons.\"\n")
-
-
-def event_home(character):
-    print(f"\nMom \"Welcome home, {character['Name']}.",
-          f"        You look tired. Take a rest.\"", sep="\n")
-    time.sleep(1)
-    if character['Pokemon']:
-        for pokemon in character['Pokemon']:
-            pokemon['HP'] = pokemon['Max HP']
-        print(f"\nPokemons have been healed!")
-    print(f"\nMom \"Take care, {character['Name']}.\"\n", sep="\n")
-
+    
 
 def collect_name():
     character_name = input("\nPlease enter your name:\n")
@@ -392,13 +85,13 @@ def generate_map_dictionary():
             elif char == '@':
                 value = [char, ""]
             elif char == '.':
-                value = [char, event_wilderness]
+                value = [char, event.event_wilderness]
             elif char == '!':
                 value = [char, event]
             elif char == 'i':
-                value = [char, event_information]
+                value = [char, event.event_information]
             elif char == 'H':
-                value = [char, event_home]
+                value = [char, event.event_home]
             else:
                 value = [char, ""]
 
@@ -513,22 +206,6 @@ def move_character(character, direction):
     character['Location'] = new_location
 
 
-def execute_event(map_dic, character):
-    event_function = map_dic[character['Location']][1]
-    need_map_and_character = [event]
-    need_map = []
-    need_character = [event_home, event_wilderness, event_information]
-
-    if event_function in need_map_and_character:
-        event_function(map_dic, character)
-    elif event_function in need_map:
-        event_function(map_dic)
-    elif event_function in need_character:
-        event_function(character)
-    else:
-        event_function()
-
-
 def game():
     character_name = collect_name()
     character = make_character(character_name)
@@ -575,7 +252,7 @@ def game():
             move_character(character, user_choice)
             describe_current_location(map_dic, character)
             if map_dic[character['Location']][1]:
-                execute_event(map_dic, character)
+                event.execute_event(map_dic, character)
                 describe_current_location(map_dic, character)
         else:
             print("\nYou cannot go this way. Please try it again.\n")
