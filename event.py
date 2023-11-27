3ca315e7c4251d2b81f02566aa8783da23908228
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-11-27 03:12:24 -0800

    UPDATE adventure_preparation() and lion_gate_bridge() so map_dic isn't modified

--- event.py
+++ event.py
@@ -100,53 +100,55 @@ def event_path(character):
         pass
 
 
-def adventure_preparation(map_dic, character):
-    print(f"\nDr.Nabil \"Hi {character['Name']}!",
-          "         The outside is full of Pokémon. They sometimes attack you!",
-          "         So, you should bring your own Pokémon.",
-          "         Now, I give you a Pokémon.",
-          "         Which one do you choose?\"", sep="\n")
-    numbers_expected = ["1", "2", "3", "4"]
-    make_decision = False
-    while not make_decision:
-        print("\nHere are four Pokémon: 1) Bulbasaur, 2) Charmander, 3) Squirtle, 4) Pikachu\n")
-        user_input = input("Please enter your choice:\n")
-        if user_input in numbers_expected:
-            pokemon_ascii_art = characters.poke_dex()[int(user_input)]['Ascii art']
-            print(pokemon_ascii_art)
-            print(f"Are you sure to choose {characters.poke_dex()[int(user_input)]['Name']}?")
-            print("1) Yes 2) No\n")
-            user_confirm = input("Please enter your choice:\n")
-            if user_confirm == "1":
-                print(f"\nDr.Chris \"Good choice! {character['Name']}!\"")
-                battle.append_pokemon(character, int(user_input), 5, battle.calculate_max_hp(int(user_input), 5))
-                print(f"You've gotten {characters.poke_dex()[int(user_input)]['Name']}!\n")
-                make_decision = True
-                map_dic[character['Location']] = " "  # update map character
-            else:
-                continue
-        else:
-            print("\nYou're choice is not valid. Please try it again.\n")
-    print(f"\nDr.Nabil \"Take these, too.\"")
-    character['Item']['Potion'] = 3
-    print(f"You've gotten three 'Potion'!")
-    character['Item']['Poke Ball'] = 5
-    print(f"You've gotten five 'Poke Ball'!")
-    print(f"\nDr.Nabil \"You can use 'Potion' to heal your Pokémon.",
-          f"         'Poke Ball' is used to catch wild Pokémon during battle.",
-          f"         However, to catch a Pokémon for sure, you need to weaken it before throwing a Poké Ball\"\n",
-          sep="\n")
-    input("Press Enter to continue...\n")
-    print(f"\nDr.Nabil \"Now, you're ready to go on an adventure!\"\n",
-          f"         \"To go to BCIT Pokémon Gym, you should pass through Lion Gate Bridge.\"\n",
-          f"         \"The construction worker at the bridge wouldn't let you pass through unless you have at least "
-          f"three Pokémon with you.\"\n")
-    character['Trainer rank'] = 1
-    print(f"Your trainer rank has increased to {character['Trainer rank']}!")
-    character['Next goal'] = "Let's catch two more Pokémon and go to Lion Gate Bridge!"
-
-
 # invoked by event()
+def adventure_preparation(map_dic, character):
+    if character['Trainer rank'] > 1:
+        print(f"\nDr.Nabil \"Hi {character['Name']}!\"")
+    else:
+        print(f"\nDr.Nabil \"Hi {character['Name']}!",
+              "         The outside is full of Pokémon. They sometimes attack you!",
+              "         So, you should bring your own Pokémon.",
+              "         Now, I give you a Pokémon.",
+              "         Which one do you choose?\"", sep="\n")
+        numbers_expected = ["1", "2", "3", "4"]
+        make_decision = False
+        while not make_decision:
+            print("\nHere are four Pokémon: 1) Bulbasaur, 2) Charmander, 3) Squirtle, 4) Pikachu\n")
+            user_input = input("Please enter your choice:\n")
+            if user_input in numbers_expected:
+                pokemon_ascii_art = characters.poke_dex()[int(user_input)]['Ascii art']
+                print(pokemon_ascii_art)
+                print(f"Are you sure to choose {characters.poke_dex()[int(user_input)]['Name']}?")
+                print("1) Yes 2) No\n")
+                user_confirm = input("Please enter your choice:\n")
+                if user_confirm == "1":
+                    print(f"\nDr.Chris \"Good choice! {character['Name']}!\"")
+                    battle.append_pokemon(character, int(user_input), 5, battle.calculate_max_hp(int(user_input), 5))
+                    print(f"You've gotten {characters.poke_dex()[int(user_input)]['Name']}!\n")
+                    make_decision = True
+                else:
+                    continue
+            else:
+                print("\nYou're choice is not valid. Please try it again.\n")
+        print(f"\nDr.Nabil \"Take these, too.\"")
+        character['Item']['Potion'] = 3
+        print(f"You've gotten three 'Potion'!")
+        character['Item']['Poke Ball'] = 5
+        print(f"You've gotten five 'Poke Ball'!")
+        print(f"\nDr.Nabil \"You can use 'Potion' to heal your Pokémon.",
+              f"         'Poke Ball' is used to catch wild Pokémon during battle.",
+              f"         However, to catch a Pokémon for sure, you need to weaken it before throwing a Poké Ball\"\n",
+              sep="\n")
+        input("Press Enter to continue...\n")
+        print(f"\nDr.Nabil \"Now, you're ready to go on an adventure!\"\n",
+              f"         \"To go to BCIT Pokémon Gym, you should pass through Lion Gate Bridge.\"\n",
+              f"         \"The construction worker at the bridge wouldn't let you pass through unless you have at least "
+              f"three Pokémon with you.\"\n")
+        character['Trainer rank'] = 1
+        print(f"Your trainer rank has increased to {character['Trainer rank']}!")
+        character['Next goal'] = "Let's catch two more Pokémon and go to Lion Gate Bridge!"
+
+
 def lonsdale_quay(character):
     print("\n\"Here is 'Lonsdale Quay', the north shore terminal of 'SeaBus'.\"")
     if 'SeaBus Ticket' in character['Item']:
@@ -158,7 +160,9 @@ def lonsdale_quay(character):
 
 
 def lion_gate_bridge(map_dic, character):
-    if len(character['Pokemon']) < 6:
+    if character['Trainer rank'] > 1:
+        print(f"\nConstruction Worker Sam \"Hi {character['Name']}!\"")
+    elif len(character['Pokemon']) < 6:
         print(f"\nConstruction Worker Sam \"Sorry, you cannot proceed unless you have at least three Pokémon with "
               f"you.\"")
         character["Location"] = (5, 8)
@@ -171,7 +175,6 @@ def lion_gate_bridge(map_dic, character):
         win_battle = battle.battle_with_trainer(character, sam)
         if win_battle:
             print("\nConstruction Worker Sam \"Passable!\"")
-            map_dic[character['Location']] = " "  # update map character
         else:
             go_home(character)
 
