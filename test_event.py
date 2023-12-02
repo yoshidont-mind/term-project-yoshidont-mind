4dffec0d8064f62504877acfdcd7087cc2c365e7
yoshidont-mind <tyoshida6@my.bcit.ca>
2023-12-02 04:30:06 -0800

    COMPLETE test_event.py

---
+++ test_event.py
@@ -0,0 +1,15 @@
+import io
+from unittest import TestCase
+from unittest.mock import patch
+
+from event import event
+
+
+class TestEvent(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_event(self, mock_output):
+        character = {'Name': 'Tats', 'Location': (5, 9), 'Trainer rank': 2}
+        expected = "\nConstruction Worker Sam \"Hi Tats!\"\n"
+        event(character)
+        actual = mock_output.getvalue()
+        self.assertIn(expected, actual)
